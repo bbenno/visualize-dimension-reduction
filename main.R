c7569f5d70f9b252bb17c59110e0b9f909e733b2
Benno Bielmeier <github@bbenno.com>
2020-12-30 11:40:01 +0100

    Add main.R

---
+++ main.R
@@ -0,0 +1,28 @@
+library(readr)
+library(Rtsne)
+library(here)
+
+# To be defined...
+dimension_data_file <- "<...>.tsv"
+label_file <- "<...>.tsv"
+
+# Import TSV file
+dimension_data <- read_tsv(here("data", dimension_data_file), col_names = FALSE)
+labels <- read_tsv(here("data", label_file), col_names = FALSE)
+data <- as.matrix(dimension_data)
+
+# Calculate T-SNE
+set.seed(42)
+tsne_out <- Rtsne(
+  data,
+  perplexity = min(30, as.integer(nrow(embeddings - 1) / 3))
+)
+
+# Plot T-SNE
+data.frame(
+  x = tsne_out$Y[, 1],
+  y = tsne_out$Y[, 2],
+  col = labels$X1
+) %>%
+  ggplot() +
+  geom_point(aes(x = x, y = y, color = col))
